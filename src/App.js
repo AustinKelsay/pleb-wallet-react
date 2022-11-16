580916dc5d70ec0a5811363abb80d9bc30deaf1d
austinkelsay <austinkelsay@yahoo.com>
2022-11-16 17:23:57 -0600

    initial

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import axios from "axios";
 
 function App() {
+  const [price, setPrice] = useState(0);
+
+  const getPrice = () => {
+    axios
+      .get("https://api.coinbase.com/v2/prices/BTC-USD/spot")
+      // .then is a promise that will run when the API call is successful
+      .then((res) => {
+        setPrice(res.data.data.amount);
+      })
+      // .catch is a promise that will run if the API call fails
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  useEffect(() => {
+    getPrice();
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Bitcoin Price</h1>
+      <h3>${price}</h3>
     </div>
   );
 }
