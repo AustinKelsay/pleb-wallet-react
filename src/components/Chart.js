3d584feba7b07ad4913103d1dc8aac334be4f475
austinkelsay <austinkelsay@yahoo.com>
2022-12-17 12:12:34 -0600

    test commit

--- src/components/Chart.js
+++ src/components/Chart.js
@@ -17,7 +17,7 @@ const Chart = ({ chartData }) => {
     <div className="chart-container">
       {chartData && chartData.length <= 1 ? (
         // ToDo: Add a loading spinner
-        <p>Loading</p>
+        <p>loading</p>
       ) : (
         <LineChart
           xLabel="Time"
